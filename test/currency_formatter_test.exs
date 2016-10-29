7dfb55d43a42b71722df6abff64b211bf5861cd1
optikfluffel <optik@fluffel.io>
2016-10-29 13:42:40 +0200

    update currency_iso.json
    including (doc-)tests and examples

--- test/currency_formatter_test.exs
+++ test/currency_formatter_test.exs
@@ -3,26 +3,26 @@ defmodule CurrencyFormatterTest do
   doctest CurrencyFormatter
   test "should correctly format in euro and dollars" do
     assert "€0,01" = CurrencyFormatter.format(1, :eur)
-    assert "$0.01" = CurrencyFormatter.format(1, "USD")
+    assert "US$0.01" = CurrencyFormatter.format(1, "USD")
     assert "0,01Ft" = CurrencyFormatter.format(1, "huf")
     assert "A$0.01" = CurrencyFormatter.format(1, :AUD)
   end
 
   test "should format an amount_in_cents as integer to a price in euro" do
-    assert "$0.01" = CurrencyFormatter.format(1)
-    assert "$0.12" = CurrencyFormatter.format(12)
-    assert "$1.23" = CurrencyFormatter.format(123)
-    assert "$12.34" = CurrencyFormatter.format(1234)
-    assert "$123.45" = CurrencyFormatter.format(12345)
-    assert "$1,234.56" = CurrencyFormatter.format(123456)
-    assert "$1,000" = CurrencyFormatter.format(100000)
-    assert "$-1,000" = CurrencyFormatter.format(-100000)
-    assert "$12,345.67" = CurrencyFormatter.format(1234567)
-    assert "$-12,345.67" = CurrencyFormatter.format(-1234567)
-    assert "$123,456.78" = CurrencyFormatter.format(12345678)
-    assert "$1,234,567.89" = CurrencyFormatter.format(123456789)
-    assert "$12,345,678.90" = CurrencyFormatter.format(1234567890)
-    assert "$1" = CurrencyFormatter.format(100)
+    assert "US$0.01" = CurrencyFormatter.format(1)
+    assert "US$0.12" = CurrencyFormatter.format(12)
+    assert "US$1.23" = CurrencyFormatter.format(123)
+    assert "US$12.34" = CurrencyFormatter.format(1234)
+    assert "US$123.45" = CurrencyFormatter.format(12345)
+    assert "US$1,234.56" = CurrencyFormatter.format(123456)
+    assert "US$1,000" = CurrencyFormatter.format(100000)
+    assert "US$-1,000" = CurrencyFormatter.format(-100000)
+    assert "US$12,345.67" = CurrencyFormatter.format(1234567)
+    assert "US$-12,345.67" = CurrencyFormatter.format(-1234567)
+    assert "US$123,456.78" = CurrencyFormatter.format(12345678)
+    assert "US$1,234,567.89" = CurrencyFormatter.format(123456789)
+    assert "US$12,345,678.90" = CurrencyFormatter.format(1234567890)
+    assert "US$1" = CurrencyFormatter.format(100)
   end
 
   test "should format an amount_in_cents as integer to a price in dollars" do
@@ -43,21 +43,21 @@ defmodule CurrencyFormatterTest do
   end
 
   test "should format an amount_in_cents as string to a price" do
-    assert "$0.01" = CurrencyFormatter.format("1")
-    assert "$0.12" = CurrencyFormatter.format("12")
-    assert "$1.23" = CurrencyFormatter.format("12.3")
-    assert "$12.34" = CurrencyFormatter.format("12.34")
-    assert "$123.45" = CurrencyFormatter.format("123,45")
-    assert "$1,234.56" = CurrencyFormatter.format("1234,56")
-    assert "$12,345.67" = CurrencyFormatter.format("12345.67")
-    assert "$123,456.78" = CurrencyFormatter.format("123456,78")
-    assert "$1,234,567.89" = CurrencyFormatter.format("1234567.89")
-    assert "$12,345,678.90" = CurrencyFormatter.format("12345678,90")
-    assert "$1" = CurrencyFormatter.format("1.00")
+    assert "US$0.01" = CurrencyFormatter.format("1")
+    assert "US$0.12" = CurrencyFormatter.format("12")
+    assert "US$1.23" = CurrencyFormatter.format("12.3")
+    assert "US$12.34" = CurrencyFormatter.format("12.34")
+    assert "US$123.45" = CurrencyFormatter.format("123,45")
+    assert "US$1,234.56" = CurrencyFormatter.format("1234,56")
+    assert "US$12,345.67" = CurrencyFormatter.format("12345.67")
+    assert "US$123,456.78" = CurrencyFormatter.format("123456,78")
+    assert "US$1,234,567.89" = CurrencyFormatter.format("1234567.89")
+    assert "US$12,345,678.90" = CurrencyFormatter.format("12345678,90")
+    assert "US$1" = CurrencyFormatter.format("1.00")
   end
 
   test "should use a currency symbol when given" do
-    assert "$1,234,567.89" = CurrencyFormatter.format(123456789, :USD)
+    assert "US$1,234,567.89" = CurrencyFormatter.format(123456789, :USD)
   end
 
   test "should return a map with formatting instructions do" do
